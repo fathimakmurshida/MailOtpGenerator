a61391792b8bb7026975ec9bb3dff5b8e964355d
fathimakmurshida <46109003+fathimakmurshida@users.noreply.github.com>
2022-04-11 17:55:43 +0530

    subject added

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def send_otp(emailid):
         for i in range(6):
             OTP+=digits[math.floor(random.random()*10)]
         otp = OTP + " is your OTP"
-        msg= otp
+        subject='r one v one'
+        msg= f'Subject: {subject}\n\n{otp}'
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
         s.login("fathimamurshida95@gmail.com", "escxawwmkp")
