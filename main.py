1a44ada9ed56e27dfd3b6fc166379c70f9dd30be
fathimakmurshida <46109003+fathimakmurshida@users.noreply.github.com>
2022-04-11 17:12:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import string
+from  fastapi import FastAPI
+import math
+import random
+import smtplib
+
+
+app= FastAPI()
+
+
+
+
+@app.post("/email-verification/{emailid}")
+def send_otp(emailid):
+        
+        digits="0123456789"
+        OTP=""
+        for i in range(6):
+            OTP+=digits[math.floor(random.random()*10)]
+        otp = OTP + " is your OTP"
+        msg= otp
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s.starttls()
+        s.login("fathimamurshida95@gmail.com", "ffuwziescxawwmkp")
+        s.sendmail('fathimamurshida95@gmail.com',emailid,msg)
+        return("success")
+   
